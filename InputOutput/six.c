e560922a6c7615043930d3df1b57f70fcce56753
Manoj Dhage <manojdhage1341@gmail.com>
2025-08-17 18:22:57 +0530

    printf("%s",str);

---
+++ InputOutput/six.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[10];
+    scanf("%s",str);
+    printf("%s",str);
+    return 0;
+}
+
+// This code reads a string from the input and prints it to the output.
+// The string is stored in a character array of size 10.
