577ef7e9aa34cbcb0fd09e3fc4ecc98917e3cb47
Manoj Dhage <manojdhage1341@gmail.com>
2025-08-17 17:55:35 +0530

    two integers from input and prints

---
+++ InputOutput/one.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d %d",a,b);
+
+    return 0;
+}
+// This code reads two integers from input and prints them.
