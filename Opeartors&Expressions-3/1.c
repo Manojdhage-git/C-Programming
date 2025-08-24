be89c9732f5f3166c9af430291d9ebb56cbe645c
Manoj Dhage <manojdhage1341@gmail.com>
2025-08-24 18:25:00 +0530

    size of operator

---
+++ Opeartors&Expressions-3/1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ 	printf("\n %d", sizeof(int));
+ 	
+ 	int a;
+ 	printf("\n %d", sizeof(a));
+ 	
+ 	printf("\n %d", sizeof(2));
+ 	
+	return 0;
+}
+// sizeof is a compile time operator which returns size of data type or variable or constant in bytes.
