9c01ff614dfc4ba2ae6435956c0dc62d6641ff2c
Manoj Dhage <manojdhage1341@gmail.com>
2025-08-24 17:58:34 +0530

    /*Integer arithmetic operations*/

---
+++ Opeartors&Expressions/1.c
@@ -0,0 +1,24 @@
+/*Integer arithmetic 
+operations*/
+
+#include<stdio.h>
+int main()
+{
+   	int x=-3, y=+4; //Unary operators
+   	printf("%d %d \n", x,y);
+   	
+   	int a=17,b=4;
+   	
+   	
+	printf("Sum=%d \n",a+b);
+	printf("Difference=%d \n",a-b);
+	printf("Product=%d \n", a*b);
+	printf("Quotient=%d \n", a/b);
+	printf("Remainder=%d \n", a%b);	
+	return 0;
+}
+
+
+
+/*Output:
+-3 4   */
