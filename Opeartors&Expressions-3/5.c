d086b38037509d0090ff517cf27161aab089a600
Manoj Dhage <manojdhage1341@gmail.com>
2025-08-24 18:30:36 +0530

    bitwise opeartor

---
+++ Opeartors&Expressions-3/5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{   
+    int a = 20;
+    int b = 10;
+    int c = a&b;
+    int d = a|b;
+    int e = a^b;
+    
+    printf("c=%d \t d=%d \t e=%d",c,d,e);
+    
+    return 0;
+}
+
+
+// Output: c=0 	 d=30 	 e=30
