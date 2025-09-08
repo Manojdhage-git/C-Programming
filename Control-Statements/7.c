cad3233a4cdc44e9da91540890ec11fd8ebd0fbc
Manoj Dhage <manojdhage1341@gmail.com>
2025-09-08 21:27:43 +0530

    control statement

---
+++ Control-Statements/7.c
@@ -0,0 +1,18 @@
+/*Program to print the sum of digits of any number*/
+#include<stdio.h>
+int main()
+{
+	int n, sum=0, rem;
+	printf("Enter a number : ");
+	scanf("%d",&n);
+	while(n>0)
+	{
+		rem = n%10;	/*taking last digit of n*/
+		sum = sum+rem;
+		n = n/10;		/*skipping last digit of n*/
+	}
+	printf("Sum of digits=%d\n",sum);
+	return 0;
+}
+
+
