0c383aa0a76c03788e9f6496d2f3132436328121
Manoj Dhage <manojdhage1341@gmail.com>
2025-09-08 21:32:36 +0530

    control statement

---
+++ Control-Statements-2/7.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+int main()
+{switch ('c')
+ {
+    case 'a':
+    case 'e':
+    case 'i' :
+    case 'o':
+    case 'u':
+      printf(" Vowel character");
+      break;
+    default : 
+      printf("Not a Vowel character"); 
+      break;    
+  } 
+}
+
