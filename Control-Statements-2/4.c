2b5a2c25c28a8c3ce17d6558b980fae9ee2246a9
Manoj Dhage <manojdhage1341@gmail.com>
2025-09-08 21:32:16 +0530

    control statement

---
+++ Control-Statements-2/4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>  
+int main()
+{
+    int x=1;
+    switch(x)
+    {
+        case 2: printf("Choice is 1");
+            break;
+        case 1+1: printf("Choice is 2");
+            break;
+        default: printf("default choice");
+    }
+return 0;
+}    
+
