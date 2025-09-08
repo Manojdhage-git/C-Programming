55cf068079ecf09d95d7d367eb23c8111f64e381
Manoj Dhage <manojdhage1341@gmail.com>
2025-09-08 21:27:14 +0530

    control statement

---
+++ Control-Statements/4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	float m1,m2,m3,m4,total,per;
+	char grade;
+	printf("Enter marks of 4 subjects : ");
+	scanf("%f%f%f%f",&m1,&m2,&m3,&m4);
+	total=m1+m2+m3+m4;
+
+	per=total/4;
+	if(per>=85)
+		grade='A';
+	else if(per>=70)
+		grade='B';
+	else if(per>=55)
+		grade='C';
+	else if(per>=40)
+		grade='D';
+	else
+		grade='E';
+	printf("Percentage is %f,Grade is %c\n",per,grade);
+	return 0;
+}
+
