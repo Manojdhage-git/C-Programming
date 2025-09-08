f297e9249cb22db7945d72aa63e82530ae4e211a
Manoj Dhage <manojdhage1341@gmail.com>
2025-09-08 21:32:22 +0530

    control statement

---
+++ Control-Statements-2/5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>  
+int main()
+{
+    float x=1.1;
+    switch(x)
+    {
+        case 1.1: printf("Choice is 1");
+            break;
+        default: printf("Invalid Choice");
+            break;
+    }
+return 0;
+}    
+
