59805ab3181713e76a89e7221a867b428af9caea
Manoj Dhage <manojdhage1341@gmail.com>
2025-09-19 18:09:25 +0530

    Add program to input and output strings using scanf() and printf()

---
+++ String/2.c
@@ -0,0 +1,13 @@
+/*Input and output strings using scanf() and printf()*/ 
+
+#include<stdio.h>
+int main()
+{
+	char name[40];
+	printf("Enter a name : ");
+	scanf("%s",name);
+	printf("%s %s\n",name,"Course"); 
+	return 0;
+}
+
+
